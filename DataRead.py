9508cc91488cff487d5cbfbbb9aaac5238f6abaf
Ryan Robinson <ryan.robinson@Vescent.local>
2020-07-31 16:04:56 -0600

    updating files

--- DataRead.py
+++ DataRead.py
@@ -118,45 +118,61 @@ class RampCollect:
         print(self.SvO)
         self.rampPoints = 256
         self.IB.sendCommandR('RampNum '+str(self.rampPoints))
-        self.IB.sendCommandR('RampSwp 1')
-        self.logfolder = str('\\'.join([os.getcwd(),'IceTracePlots',time.strftime("%Y-%m-%d_%H-%M-%S")]))
+        self.rampSweep = 0.5
+        self.IB.sendCommandR('RampSwp '+str(self.rampSweep))
+        self.logfolder = str('\\'.join([os.getcwd(),'IceTracePlots',time.strftime("%Y-%m-%d_%H-%M-%S"),'UnstitchedData']))
+        self.logfolderStitched = str('\\'.join([os.getcwd(),'IceTracePlots',time.strftime("%Y-%m-%d_%H-%M-%S"),'StitchedData']))
+        self.logfolderPlots = str('\\'.join([os.getcwd(),'IceTracePlots',time.strftime("%Y-%m-%d_%H-%M-%S"),'Plots']))
         os.makedirs(self.logfolder)
+        os.makedirs(self.logfolderStitched)
+        os.makedirs(self.logfolderPlots)
         print("Save Directory: "+self.logfolder)
+        self.xAxisTotal = []
+        self.traceDataTotal = []
+        self.derDataTotal = []
         return None
     
     def getData(self):
-        '''Runs the ramp, collects the data, and converts it to volts'''
+        '''
+        Runs the ramp, collects the data, and converts it to volts
+        derData: derivative trace derived from converting fm to am
+        traceData: absorption trace
+        '''
         toVolts = 3.05*(10**3)
         self.IB.sendCommandR('RampRun')
-        self.topData,self.botData = self.IB.bulkRead()
-        self.topData = self.topData/toVolts
-        self.botData = self.botData/toVolts
-        return self.topData,self.botData
+        self.derData,self.traceData = self.IB.bulkRead()
+        self.derData = self.derData/toVolts
+        self.traceData = self.traceData/toVolts
+        return self.derData,self.traceData
     
     def setSvOffset(self,offst = 0.05):
         '''Sets the servo offset'''
         self.SvO = self.IB.sendCommandR('SvOffst '+str(offst))
         return self.SvO
-        
-    
+   
     def setCurrent(self,current):
         '''Sets the laser current'''
         self.Curr = self.IB.sendCommandR('CurrSet 101.12')
         return self.Curr
     
-    def plotGraphs(self,offset = 0):
-        '''Creates two graphs, input the servo offset to calculate the shift'''
-        self.xAxis = np.arange(offset,offset+self.rampPoints)
-        self.stripDataEnds()
+    def plotGraphs(self,):
+        '''Creates two graphs, input the servo offset to calculate the shift'''       
         plt.figure(1)
-        plt.title("Trace Derivative")
+        plt.title("Raw ICE Derivative Trace")
         plt.ylabel("DC Error [V]")
-        plt.plot(self.xAxis,self.topData)
+        plt.xlabel("Ramp Voltage [V]")
+        plt.plot(self.xAxis,self.derData)
         plt.figure(2)
-        plt.title("Trace")
+        plt.title("Raw ICE Trace")
         plt.ylabel("Error Input [V]")
-        plt.plot(self.xAxis,self.botData)
+        plt.xlabel("Ramp Voltage [V]")
+        plt.plot(self.xAxis,self.traceData)
         return None
+    
+    def genXAxis(self,SvO):
+        sFactor = 0.83 # Determined expirementally to overlap plots
+        self.xAxis = np.linspace(SvO - sFactor*self.rampSweep/2,SvO + sFactor*self.rampSweep/2,self.rampPoints)
+        return self.xAxis
     
     def stripDataEnds(self):
         '''Remove artifacts from data'''
@@ -165,17 +181,8 @@ class RampCollect:
         dStart = sFactor
         dEnd = dataLen - sFactor - 1
         self.xAxis = self.xAxis[dStart:dEnd]
-        self.topData = self.topData[dStart:dEnd]
-        self.botData = self.botData[dStart:dEnd]
-    
-    def takeTrace(self,offst = 0.05):
-        '''Sets the servo offset and takes a trace'''
-        self.setSvOffset(offst)
-        sFactor = 0.84
-        graphOffset = int(offst * self.rampPoints / sFactor)
-        self.getData()
-        self.saveData()
-        self.plotGraphs(graphOffset)
+        self.derData = self.derData[dStart:dEnd]
+        self.traceData = self.traceData[dStart:dEnd]
         return None
     
     def saveData(self):
@@ -184,12 +191,99 @@ class RampCollect:
         s = s*10
         s = round(s,2)
         s = str(s).replace(".","")
-        a = self.logfolder+"\\topPlot"+s+".csv"
-        b = self.logfolder+"\\bopPlot"+s+".csv"
-        np.savetxt(a, self.topData, delimiter=",")
-        np.savetxt(b, self.botData, delimiter=",")
+        a = self.logfolder+"\\rawIceDerivative"+s+".csv"
+        b = self.logfolder+"\\rawIceTrace"+s+".csv"
+        np.savetxt(a, (self.derData), delimiter=",")
+        np.savetxt(b, (self.traceData), delimiter=",")
         return None
     
+    def appendData(self):
+        '''Appends the data so that we may work with it as 1 big array into several smaller arrays. 
+           Must be exectued AFTER completion of offset scan loops'''
+        self.xAxisTotal = np.append(self.xAxisTotal,self.xAxis)
+        self.derDataTotal = np.append(self.derDataTotal,self.derData)
+        self.traceDataTotal = np.append(self.traceDataTotal,self.traceData)
+        return None
+    
+    def modifyAppendData(self):
+        '''Reverses and flips the plots to make them more readable'''
+        self.traceDataTotalMod = np.flip(-1*self.traceDataTotal)
+        self.derDataTotalMod = np.flip(self.derDataTotal)
+        return None
+        
+    
+    def plotAppendData(self):
+        plt.figure(3)
+        plt.title("Raw ICE Total Derivative Trace")
+        plt.ylabel("DC Error [V]")
+        plt.xlabel("Ramp Voltage [V]")
+        plt.plot(self.xAxisTotal,self.derDataTotal)
+        plt.figure(4)
+        plt.title("Raw Total ICE Trace")
+        plt.ylabel("Error Input [V]")
+        plt.xlabel("Ramp Voltage [V]")
+        plt.plot(self.xAxisTotal,self.traceDataTotal)
+        plt.figure(5)
+        plt.title("Mod Total ICE Derivative Trace")
+        plt.ylabel("Error Input [V]")
+        plt.xlabel("Ramp Voltage [V]")
+        plt.plot(self.xAxisTotal,self.derDataTotalMod)
+        plt.figure(6)
+        plt.title("Mod Total ICE Trace")
+        plt.ylabel("Error Input [V]")
+        plt.xlabel("Ramp Voltage [V]")
+        plt.plot(self.xAxisTotal,self.traceDataTotalMod)        
+        return None
+        
+    def saveAppendData(self):
+        '''Saves the appended data in a folder with the data labeled'''
+        a = self.logfolderStitched+"\\total-raw-derivative-data-set.csv"
+        b = self.logfolderStitched+"\\total-raw-trace-data-set.csv"
+        c = self.logfolderStitched+"\\total-modified-derivative-data-set.csv"
+        d = self.logfolderStitched+"\\total-modified-trace-data-set.csv"
+        e = self.logfolderStitched+"\\total-raw-xAxis.csv"
+        f = self.logfolderStitched+"\\total-modified-xAxis.csv"
+        np.savetxt(a, (self.derDataTotal), delimiter=",")
+        np.savetxt(b, (self.traceDataTotal), delimiter=",")    
+        np.savetxt(c, (self.traceDataTotalMod), delimiter=",") 
+        np.savetxt(d, (self.traceDataTotalMod), delimiter=",")
+        np.savetxt(e, (self.xAxisTotal), delimiter=",") 
+        return None
+    
+    def takeTrace(self,offst = 0.05):
+        '''Sets the servo offset and takes a trace'''
+        self.setSvOffset(offst)
+        self.genXAxis(offst)
+        self.getData()
+        self.saveData()
+        self.stripDataEnds()
+        self.appendData()
+        self.plotGraphs()
+        return None    
+    
+    def postLoopDataProcessing(self):
+        '''Run only after all data has been aquired'''
+        self.modifyAppendData()
+        self.saveAppendData()
+        self.plotAppendData()
+        self.savePlots()
+        return None
+    
+    def savePlots(self):
+        plt.figure(1)
+        plt.savefig(self.logfolderPlots+"\\raw-stitched-derivative.png")
+        plt.figure(2)
+        plt.savefig(self.logfolderPlots+"\\raw-stitched-traces.png")
+        plt.figure(3)
+        plt.savefig(self.logfolderPlots+"\\total-raw-derivative.png")
+        plt.figure(4)
+        plt.savefig(self.logfolderPlots+"\\total-raw-traces.png")
+        plt.figure(5)
+        plt.savefig(self.logfolderPlots+"\\total-modified-derivative.png")    
+        plt.figure(6)
+        plt.savefig(self.logfolderPlots+"\\total-modified-traces.png")   
+        return None
+        
     def rIceClose(self):
         self.IB.IceClose()
         return None
@@ -202,17 +296,15 @@ def main():
     IB = ICE(BoxNum,Cs1SlotNum)
     try:
         RC = RampCollect(IB)
-        
-#        RC.takeTrace(0)
-        
-        servoOffsets = np.arange(-4.5,2.5,.5)
+        servoOffsets = np.arange(-4.5,2.5,.25)
         for i in servoOffsets:
             RC.takeTrace(i)
-        
+        RC.postLoopDataProcessing()
         IB.IceClose()
     except:
-        print('fail')
-        IB.IceClose()    
+        print("Error:", sys.exc_info()[0])
+        IB.IceClose()
+    return None
     
 if(__name__=="__main__"):    
     main()
